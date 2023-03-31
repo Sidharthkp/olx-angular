6450d9c32a6f5c88f05ba8c19f24ee38269710a5
Sidharthkp <sidharthkannan20@gmail.com>
2023-03-31 17:48:07 +0530

    Added tailwind for styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LocationComponent } from './components/location/location.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule
