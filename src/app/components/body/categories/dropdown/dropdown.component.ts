76ef8a9a6272a91ddd6de3e373e57395624adf1d
Sidharthkp <sidharthkannan20@gmail.com>
2023-04-01 15:22:20 +0530

    Going to items styling

--- src/app/components/body/categories/dropdown/dropdown.component.ts
+++ src/app/components/body/categories/dropdown/dropdown.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-dropdown',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./dropdown.component.css']
 })
 export class DropdownComponent {
-
+  faAngleDown = faAngleDown; 
 }
