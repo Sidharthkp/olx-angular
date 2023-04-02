281f4a872ae80d4a96d9791437745b9f345bd2a2
Sidharthkp <sidharthkannan20@gmail.com>
2023-04-02 13:03:09 +0530

    routing done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
@@ -17,12 +18,18 @@ import { CategoryComponent } from './components/body/categories/category/categor
 import { BannerComponent } from './components/body/banner/banner.component';
 import { ItemsComponent } from './components/body/items/items.component';
 import { LocationdropdownComponent } from './components/header/location/locationdropdown/locationdropdown.component';
-import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
-import { provideAuth,getAuth } from '@angular/fire/auth';
-import { provideDatabase,getDatabase } from '@angular/fire/database';
-import { provideFirestore,getFirestore } from '@angular/fire/firestore';
-import { provideStorage,getStorage } from '@angular/fire/storage';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideDatabase, getDatabase } from '@angular/fire/database';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+import { CreateComponent } from './components/create/create.component';
+
+const appRoutes: Routes = [
+  { path: '', component: BodyComponent },
+  { path: 'sell', component: CreateComponent }
+]
 
 @NgModule({
   declarations: [
@@ -40,7 +47,8 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     CategoryComponent,
     BannerComponent,
     ItemsComponent,
-    LocationdropdownComponent
+    LocationdropdownComponent,
+    CreateComponent
   ],
   imports: [
     BrowserModule,
@@ -49,7 +57,8 @@ import { provideStorage,getStorage } from '@angular/fire/storage';
     provideAuth(() => getAuth()),
     provideDatabase(() => getDatabase()),
     provideFirestore(() => getFirestore()),
-    provideStorage(() => getStorage())
+    provideStorage(() => getStorage()),
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
