d9770dada8fed957223c2e2b944479837eb86254
Sidharthkp <sidharthkannan20@gmail.com>
2023-04-02 22:06:34 +0530

    header router links added

--- src/app/components/create/create.component.ts
+++ src/app/components/create/create.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-create',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent {
-
+  faArrowLeft = faArrowLeft;
 }
